8a8f08579659cca1a3d1733b9e4594b66c5e5605
survival8764 <82863756+survival8764@users.noreply.github.com>
2024-06-28 19:10:17 +0900

    [ADD] HP바추가
    HP바추가

--- ProjectBC/Assets/Scripts/GyeongGeon/Interface/IBehavior.cs
+++ ProjectBC/Assets/Scripts/GyeongGeon/Interface/IBehavior.cs
@@ -2,7 +2,7 @@ public interface IBehavior
 {
     void Move();
     void Attack();
-    void Damageable();
+    void Damageable(Character target, float _damage);
     bool Die();
     bool Alive();
 }
