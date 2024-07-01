3ed0604ae5c3ac97250c50812f26ce69f25fe984
ghdwl <ghdwls4654@naver.com>
2024-07-01 19:00:27 +0900

    [Add] UIManager
    UI관리를 위한 UIManager와 MenuScreen 작성

--- ProjectBC/Assets/Scripts/Managers/DBManager.cs
+++ ProjectBC/Assets/Scripts/Managers/DBManager.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using static DB;
+using static JsonHelper;
 
 public class DBManager : MonoBehaviour
-{ 
-    [SerializeField] private List<DB.CharacterBaseData> charaterBaseData = DB.GameData.characterBaseDatas;
+{
+    [SerializeField] private CharacterBaseData[] _charaterBaseDatas;
+
+    private void Start()
+    {
+        ReadJson();
+        Inspector();
+    }
+
+    private void ReadJson()
+    {
+        GameData.characterBaseData = LoadArrayJson<CharacterBaseData>("CharacterBaseData.json");
+    }
+    private void Inspector()
+    {
+        _charaterBaseDatas = GameData.characterBaseData;
+    }
+
+    
+   
 }
