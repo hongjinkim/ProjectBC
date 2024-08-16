29b67f9b045ee368e32924907d199ea13dc79819
ghdwl <ghdwls4654@naver.com>
2024-08-16 15:00:59 +0900

    [Fix] Merge 완료

--- ProjectBC/Assets/Refactoring/Scripts/UI/PopUp/DailyStore.cs
+++ ProjectBC/Assets/Refactoring/Scripts/UI/PopUp/DailyStore.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +8,35 @@ public class DailyStore : PopUp
     [Header("Buttons")]
     public Button backButton;
 
+    [Header("Texts")]
+    public TextMeshProUGUI gold;
+
+    private PlayerInfo playerInfo;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(EventType.FundsUpdated, OnGoldUpdated);
+    }
+    private void OnDisable()
+    {
+        EventManager.StopListening(EventType.FundsUpdated, OnGoldUpdated);
+    }
+
+    public override void ShowScreen()
+    {
+        base.ShowScreen();
+    }
+
     protected override void Start()
     {
         base.Start();
+        playerInfo = GameDataManager.instance.playerInfo;
+        OnGoldUpdated(null);
         backButton.onClick.AddListener(_UIManager.DailyStorePopUp.HideScreen);
+    }
+
+    private void OnGoldUpdated(Dictionary<string, object> message)
+    {
+        gold.text = playerInfo.gold.ToString();
     }
 }
